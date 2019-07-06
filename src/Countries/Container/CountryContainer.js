3966d347d0df04032f5e77a8af60bcb1d7f0a6d8
Alison Williams <alison.lauriewill@gmail.com>
2019-07-06 11:51:34 +0100

    Created 'Countries' Folder, container, list and item files

---
+++ src/Countries/Container/CountryContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CountryList from '../Components/CountryList.js'
+
+class CountryContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            countries: []
+        }
+    }
+
+
+    
+}
