5a05b7aa2d2d044d1ba89b4e02fca9f45084f5ad
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-07 21:34:23 +0100

    reviews show country. Can not add multiple of same country to countries table on db

--- src/Reviews/Containers/ReviewBox.js
+++ src/Reviews/Containers/ReviewBox.js
@@ -25,26 +25,32 @@ class ReviewBox extends Component {
     let countriesUrl = 'https://restcountries.eu/rest/v2/all?fields=name'
     fetch(countriesUrl)
       .then(res => res.json())
-      .then(countries => this.setState({countries: countries}))
+      .then(countries => this.setState({ countries: countries }))
+      .catch(err => console.error)
+
+    let reviewedCountriesUrl = 'http://localhost:8080/countries'
+    fetch(reviewedCountriesUrl)
+      .then(res => res.json())
+      .then(reviewedCountry => this.setState({ reviewedCountries: reviewedCountry._embedded.countries }))
       .catch(err => console.error)
   }
 
   postData(data) {
-    console.log(this.state.reviewedCountries)
-    if (this.state.reviewedCountries.indexOf(data.country) < 0) {
-    // if (this.prevState.reviewedCountries.includes(data.country)) {
-    fetch('http://localhost:8080/countries', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data.country)
-    })
-      .then(res => res.json())
-      .then(reviewedCountry => this.setState(prevState => {
-        return {reviewedCountries: [...prevState.reviewedCountries, reviewedCountry.name]}
-      }))
+    const countries = this.state.reviewedCountries.map(x => x.name)
+    if (countries.includes(data.country) === false) {
+
+      fetch('http://localhost:8080/countries', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data.country)
+      })
+        .then(res => res.json())
+        .then(reviewedCountry => this.setState(prevState => {
+          return { reviewedCountries: [...prevState.reviewedCountries, reviewedCountry] }
+        }))
     }
 
     fetch('http://localhost:8080/reviews', {
@@ -57,16 +63,16 @@ class ReviewBox extends Component {
     })
       .then(res => res.json())
       .then(reviewData => this.setState(prevState => {
-        return {reviews: [...prevState.reviews, reviewData]}
+        return { reviews: [...prevState.reviews, reviewData] }
       }))
-      
+
   }
 
   render() {
     return (
       <div className="comment-box">
         <h2>Add Review</h2>
-        <ReviewForm onReviewSubmit={this.postData} countries={this.state.countries}/>
+        <ReviewForm onReviewSubmit={this.postData} countries={this.state.countries} />
         <h2>Reviews</h2>
         <ReviewList data={this.state.reviews} />
       </div>
