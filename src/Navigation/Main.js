d9e70e1506bcb918c2c214f1f2793da8762556ba
Alison Williams <alison.lauriewill@gmail.com>
2019-07-08 14:07:55 +0100

    stuuuuff

--- src/Navigation/Main.js
+++ src/Navigation/Main.js
@@ -1,22 +1,38 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from "react";
 import Home from "./Home";
 import NavBar from "./NavBar";
 import './Main.css'
 import ReviewBox from "../Reviews/Containers/ReviewBox"
 import UserForm from "../Users/Components/UserForm"
-import UserBox from "../Users/Containers/UserBox"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class Main extends Component{
   constructor(props) {
     super(props)
-
+    this.state = {
+      users: [],
+      topUser: []
+    }
 
     this.postData = this.postData.bind(this);
   }
 
+  componentDidMount() {
+    let allUsersURL = "http://localhost:8080/users"
+    fetch(allUsersURL)
+      .then(res => res.json())
+      .then(userData => this.setState({ users: userData._embedded.users }))
+      .catch(err => console.err)
+
+    let topUsersURL = "http://localhost:8080/users/ranking"
+    fetch(topUsersURL)
+    .then(res => res.json())
+    .then(topUserData => this.setState({ topUsers: topUserData }))
+    .catch(err => console.err)
+  }
+
   postData(data) {
-    return fetch('http://localhost:8080/reviews', {
+    return fetch('http://localhost:8080/users', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -25,13 +41,12 @@ class Main extends Component{
       body: JSON.stringify(data)
     })
       .then(res => res.json())
-      .then(reviewData => this.setState(prevState => {
-        return {reviews: [...prevState.reviews, reviewData]}
+      .then(userData => this.setState(prevState => {
+        return {users: [...prevState.users, userData]}
       }))
-      
   }
   
-  
+
   render(){
     return(<Router>
         <main className="layout">
@@ -41,7 +56,7 @@ class Main extends Component{
         </header>
             <Switch>
                 <Route exact path="/" component={Home}/>
-                <Route path="/add-user" render={props => <UserForm onFormSubmit = {this.postData}/>}/>
+                <Route path="/add-user" render={() => <UserForm onFormSubmit = {this.postData}/>}/>
                 <Route path="/add-user" component={UserForm}/>
                 <Route path="/add-review" component={ReviewBox}/>
             </Switch>
