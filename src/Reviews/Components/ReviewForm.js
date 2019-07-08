bc04031ae41353a1277f1654265d013887fcab48
Alison Williams <alison.lauriewill@gmail.com>
2019-07-08 22:15:20 +0100

    more form styling

--- src/Reviews/Components/ReviewForm.js
+++ src/Reviews/Components/ReviewForm.js
@@ -77,16 +77,16 @@ render(){
     <main className="review-form-main">
     <h2>Submit review:</h2>
       <form className="review-form" onSubmit={this.handleSubmit}>
-      <label for="country-selector">Select country: 
+      <label for="country-selector">Select country: </label>
         <select  id="country-selector" defaultValue="default" onChange={this.handleSelectChange}>
         <option disabled value="default">Choose a country...</option>
         {this.generateOptions()}
         </select>
-      </label>
-      <label for="title">Title:
+      
+      <label for="title">Title: </label>
         <input id="title" type="text" placeholder="Write title" value={this.state.title} onChange={this.handleTitleChange}/>
-      </label>
-      <label for="rating"> Rating:
+      
+      <label for="rating"> Rating: </label>
         <div id="rating" className="rating-stars" onChange={this.handleRatingChange}>
             <input type="radio" name="rating" value = '1' />
             <input type="radio" name="rating" value = '2' />
@@ -94,14 +94,14 @@ render(){
             <input type="radio" name="rating" value = '4' />
             <input type="radio" name="rating" value = '5' />
         </div>
-      </label>
-      <label for="date">Select date:
-      <input id="date" type="date" value={this.state.date} onChange={this.handleDateChange}/>
-      </label>
-      <label for="text">Write review:
-      <textarea id="text" placeholder="Write review" value={this.state.text} onChange={this.handleTextChange}></textarea>
-      </label>
-      <input type="submit" value="Write Review"/>
+      
+      <label for="date">Select date: </label>
+        <input id="date" type="date" value={this.state.date} onChange={this.handleDateChange}/>
+      
+      <label for="text">Write review: </label>
+        <textarea id="text" placeholder="Write review" value={this.state.text} onChange={this.handleTextChange}></textarea>
+      
+        <input type="submit" value="Write Review"/>
       </form>
     </main>
 
