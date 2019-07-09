27cfabbaa6beb1156b1a56d45685a3659061b52f
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-09 14:05:34 +0100

    clean up of code

--- src/Users/Components/IndividualUserList.js
+++ src/Users/Components/IndividualUserList.js
@@ -4,7 +4,6 @@ import IndividualUser from "./IndividualUser";
 class IndividualUserList extends Component {
     render() {
         const user = this.props.data.map((user, index) => {
-            console.log(user)
             return (
 
                 <IndividualUser username={user.username} name={user.name} level={user.level} id={user.id} key={index} reviews={user.reviews} />
