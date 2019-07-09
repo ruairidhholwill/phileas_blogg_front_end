871c77284359fbe94576a9964843c9632a58dd35
Mateusz <mateusz.trajdos@gmail.com>
2019-07-09 18:35:59 +0100

    Nothih changed

--- src/Reviews/Components/Review.js
+++ src/Reviews/Components/Review.js
@@ -1,5 +1,4 @@
 import React,{Component} from "react";
-import {Link} from 'react-router-dom'
 import './ReviewItem.css';
 
 class Review extends Component{
@@ -7,46 +6,44 @@ class Review extends Component{
 
 
 render(){
-  let reviewContent = this.props.text;
+let reviewContent = this.props.text;
 
-  if(this.props.text.length > 130){
+if(this.props.text.length > 130){
 
-    let maxLength = 130;
-    let shorterReview = reviewContent.substr(0, maxLength);
-    shorterReview = shorterReview.substr(0, Math.min(shorterReview.length, shorterReview.lastIndexOf(" ")))
-      
-    return(
-      <Link to={`/reviews/${this.props.id}`}>
+  let maxLength = 130;
 
-          <div className="review">
+  let shorterReview = reviewContent.substr(0, maxLength);
+  shorterReview = shorterReview.substr(0, Math.min(shorterReview.length, shorterReview.lastIndexOf(" ")))
 
-            <h4 className="review-title"><a href={"/reviews/" + this.props.id}>{this.props.title}</a></h4>
-            <p className="review-content">{shorterReview + '...'}</p>
-            <p>Country: {this.props.country}</p>
-            <p>Rating: {this.props.rating}</p>
-            <p class="post-date">Posted: {this.props.date}</p>
 
-          </div>
-      </Link>
-  );
+return(
+
+
+        <div className="review">
+
+          <h4 className="review-title"><a href={"/reviews/" + this.props.id}>{this.props.title}</a></h4>
+          <p className="review-content">{shorterReview + '...'}</p>
+          <p>Country: {this.props.country}</p>
+          <p>Rating: {this.props.rating}</p>
+          <p class="post-date">Posted: {this.props.date}</p>
+
+        </div>
+);
 
 } else {
 
   return(
-    <Link to={`/reviews/${this.props.id}`}>
 
 
       <div className="review">
 
-        <h4 className="review-title">{this.props.title}</h4>
+        <h4 className="review-title"><a href={"/reviews/" + this.props.id}>{this.props.title}</a></h4>
         <p className="review-content">{this.props.text}</p>
         <p>Country: {this.props.country}</p>
         <p>Rating: {this.props.rating}</p>
-        <p className="post-date">Posted: {this.props.date}</p>
+        <p class="post-date">Posted: {this.props.date}</p>
       </div>
-    </Link>
-  );
-}
+);}
 
 
   }
