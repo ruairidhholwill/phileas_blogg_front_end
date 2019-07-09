1b36209dbda26926fc3ca6a963bd7f501c9a547a
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-09 21:14:09 +0100

    search bar works for searching by review title

--- src/Reviews/Containers/ReviewBox.js
+++ src/Reviews/Containers/ReviewBox.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ReviewList from "../Components/ReviewList"
 
 
@@ -35,30 +35,35 @@ class ReviewBox extends Component {
       .catch(err => console.error)
   }
 
-  handleChange(event){
-    console.log(event.target.value)
-    let reviewsUrl = `http://localhost:8080/reviews/contains/${event.target.value}`
-    fetch(reviewsUrl)
-      .then(res => res.json())
-      .then(reviewData => this.setState({ searchedReviews: reviewData }))
-      .catch(err => console.err)
-      .then(console.log(this.state.searchedReviews))
-      // debugger
+  handleChange(event) {
+    if (event.target.value.length > 0) {
+      let reviewsUrl = `http://localhost:8080/reviews/contains/${event.target.value}`
+      fetch(reviewsUrl)
+        .then(res => res.json())
+        .then(reviewData => this.setState({ reviews: reviewData }))
+        .catch(err => console.err)
+    } else if (event.target.value.length === 0) {
+      let reviewsUrl = `http://localhost:8080/reviews/all`
+      fetch(reviewsUrl)
+        .then(res => res.json())
+        .then(reviewData => this.setState({ reviews: reviewData }))
+        .catch(err => console.err)
+    }
   }
 
   render() {
     return (
       <div>
         <div>
-      <input id="search-bar" type="input" placeholder="Search.." onChange={this.handleChange}></input>
-      </div>
-      <main className="review-box">
-        <ReviewList data={this.state.reviews}/>
-      </main>
+          <input id="search-bar" type="input" placeholder="Search.." onChange={this.handleChange}></input>
+        </div>
+        <main className="review-box">
+          <ReviewList data={this.state.reviews} />
+        </main>
       </div>
     )
   }
-  
+
 }
 
 
