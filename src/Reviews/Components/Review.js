5b5b5c86a8c20cd5ee0cae01329139c076f4d8ae
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-09 11:07:45 +0100

    removed anchor from review.js

--- src/Reviews/Components/Review.js
+++ src/Reviews/Components/Review.js
@@ -8,7 +8,6 @@ class Review extends Component{
 render(){
     return(
       <Link to={`/reviews/${this.props.id}`}>
-      {/* <a href={"/reviews/" + this.props.id}> */}
         <div className="review">
           <h4>Title: {this.props.title}</h4>
           <p>Text: {this.props.text}</p>
@@ -16,7 +15,6 @@ render(){
           <p>Date: {this.props.date}</p>
           <p>Country: {this.props.country}</p>
         </div>
-     {/* </a> */}
     </Link>
     );
   }
