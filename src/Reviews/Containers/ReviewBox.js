9341d2215f586db43523a7929497908260003114
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-10 13:44:12 +0100

    refactored review box

--- src/Reviews/Containers/ReviewBox.js
+++ src/Reviews/Containers/ReviewBox.js
@@ -6,58 +6,13 @@ import NavBar from "../../Navigation/NavBar";
 class ReviewBox extends Component {
 
   constructor(props) {
-
     super(props);
-    this.state = {
-      countries: [],
-      // reviews: [],
-      reviewedCountries: [],
-      searchedReviews: []
-    };
-    // this.handleChange = this.handleChange.bind(this)
   }
-
-  componentDidMount() {
-    // let reviewsUrl = "http://localhost:8080/reviews/all"
-    // fetch(reviewsUrl)
-    //   .then(res => res.json())
-    //   .then(reviewData => this.setState({ reviews: reviewData }))
-    //   .catch(err => console.err)
-
-    let countriesUrl = 'https://restcountries.eu/rest/v2/all?fields=name'
-    fetch(countriesUrl)
-      .then(res => res.json())
-      .then(countries => this.setState({ countries: countries }))
-      .catch(err => console.error)
-
-    let reviewedCountriesUrl = 'http://localhost:8080/countries'
-    fetch(reviewedCountriesUrl)
-      .then(res => res.json())
-      .then(reviewedCountry => this.setState({ reviewedCountries: reviewedCountry._embedded.countries }))
-      .catch(err => console.error)
-  }
-
-  // handleChange(event) {
-  //   if (event.target.value.length > 0) {
-  //     let reviewsUrl = `http://localhost:8080/reviews/contains/${event.target.value}`
-  //     fetch(reviewsUrl)
-  //       .then(res => res.json())
-  //       .then(reviewData => this.setState({ reviews: reviewData }))
-  //       .catch(err => console.err)
-  //   } else if (event.target.value.length === 0) {
-  //     let reviewsUrl = `http://localhost:8080/reviews/all`
-  //     fetch(reviewsUrl)
-  //       .then(res => res.json())
-  //       .then(reviewData => this.setState({ reviews: reviewData }))
-  //       .catch(err => console.err)
-  //   }
-  // }
 
   render() {
     return (
       <div>
         <div className="search-bar-wrapper">
-      // SEARCH
         </div>
         <main className="review-box">
           <ReviewList data={this.props.reviews} />
@@ -65,8 +20,10 @@ class ReviewBox extends Component {
       </div>
     )
   }
-
 }
+
+
+
 
 
 
